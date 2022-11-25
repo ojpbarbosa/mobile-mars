e874ecab11e69b1d0e57b0dc591b409295b6e60e
Gabriel Willian Bartmanovicz <obielwb@gmail.com>
2022-11-25 12:04:05 -0300

    chore: improve recursive method

--- marte/app/src/main/java/br/unicamp/marte/AlgoritmoRecursivo.kt
+++ marte/app/src/main/java/br/unicamp/marte/AlgoritmoRecursivo.kt
@@ -1,32 +1,53 @@
 package br.unicamp.marte
 
-class AlgoritmoRecursivo (adjacencias: Array<Array<DadosCaminho?>>, cidadeInicio: Int, cidadeDestino: Int) {
-    var adjacencias: Array<Array<DadosCaminho?>> = adjacencias;
-    var cidadeInicio: Int = cidadeInicio
-    var cidadeDestino: Int = cidadeDestino
+class AlgoritmoRecursivo (adjacencias: Array<Array<DadosCaminho?>>) {
+    var adjacencias: Array<Array<DadosCaminho?>> = adjacencias
 //    var visitados = mk.zeros<Int>(adjacencias.size, adjacencias.size
 
 
-    fun executar(x: Int, y: Int, visitados: Array<Array<Boolean>>, caminho: Array<Int>, distanciaPercorrida: Int): Array<Int> {
-        if (x > 0 && x < adjacencias.size && y > 0 && y < adjacencias.size) {
-            if (adjacencias[x][y] == null) {
-                visitados[x][y] = true
+    fun executar(cidadeOrigem: Int, cidadeDestino: Int): Array<Int> {
+
+        var caminho = ArrayList<Int>()
+        var distanciaPercorrida: Int = 0
+
+        fun acharCaminho(x: Int, y: Int, distancia: Int) {
+            if (y == cidadeDestino) {
+                distanciaPercorrida
+            }
+            if (adjacencias[x][y] != null) {
+                distancia
+                acharCaminho(y, 0)
             }
             else {
-                if (x == cidadeDestino) {
-                    return caminho
-                }
-                else {
-                    if (!visitados[x][y]) {
-                        distanciaPercorrida += adjacencias[x][y]!!.distancia
-                        visitados[x][y] = true
-                        y = x
-                        x = 0
-                    }
-
-
-                }
+                acharCaminho(x + 1, y)
             }
         }
+
+        acharCaminho(0, cidadeOrigem)
+
+        return caminho;
+
+//        fun acharCaminho(x: Int, y: Int, visitados: Array<Array<Boolean>>, caminho: Array<Int>, distanciaPercorrida: Int) {}
+//
+//        if (x >= 0 && x < adjacencias.size && y >= 0 && y < adjacencias.size) {
+//            if (adjacencias[x][y] == null) {
+//                visitados[x][y] = true
+//            }
+//            else {
+//                if (x == cidadeDestino) {
+//                    return caminho
+//                }
+//                else {
+//                    if (!visitados[x][y]) {
+//                        distanciaPercorrida += adjacencias[x][y]!!.distancia
+//                        visitados[x][y] = true
+//                        y = x
+//                        x = 0
+//                    }
+//
+//
+//                }
+//            }
+//        }
     }
 }
