883932f9d48dd9dc4faf163378f4abac452558b1
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-12-04 10:18:04 -0300

    chore: delete AlgoritmoDeDijkstra class

--- marte/app/src/main/java/br/unicamp/marte/AlgoritmoDeDijkstra.kt
+++
@@ -1,9 +0,0 @@
-package br.unicamp.marte
-
-// https://github.com/alexhwoods/alexhwoods.com/blob/master/kotlin-algorithms/src/main/kotlin/com/alexhwoods/graphs/algorithms/Dijkstra.kt
-// https://www.atomiccommits.io/dijkstras-algorithm-in-kotlin
-
-class AlgoritmoDeDijkstra() {
-    // Todo("Revisar nome de variáveis")
-
-}
